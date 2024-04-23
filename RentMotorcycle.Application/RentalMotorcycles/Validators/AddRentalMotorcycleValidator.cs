318750b53c0699e69c42c1ff44a0393f3738f363
Lucas <lvs.vandresen@gmail.com>
2024-04-22 21:54:25 -0300

    Funcionalidade de aluguel de motos finalizada

---
+++ RentMotorcycle.Application/RentalMotorcycles/Validators/AddRentalMotorcycleValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using RentMotorcycle.Application.RentalMotorcycles.CommandHandlers.RentalMotorcycles;
+
+namespace RentMotorcycle.Application.RentalMotorcycles.Validators
+{
+    public class AddRentalMotorcycleValidator : AbstractValidator<AddRentalMotorcycleCommand>
+    {
+        public AddRentalMotorcycleValidator()
+        {
+            RuleFor(rentalMotorcycle => rentalMotorcycle.RentalPlanId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("O campo 'Plano' deve ser preenchido!!");
+
+            RuleFor(rentalMotorcycle => rentalMotorcycle.DeliverymanId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage("O campo 'Entregador' deve ser preenchido!!");
+        }
+    }
+}
